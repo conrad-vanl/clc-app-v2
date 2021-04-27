0eedb97ab187ed8ffbce388b51677a114fe4819c
Gordon <gburgett@watermark.org>
2021-04-27 02:29:34 -0500

    Use connection pooling to have multiple sockets open

--- apollos-church-api/src/data/cache/data-source.js
+++ apollos-church-api/src/data/cache/data-source.js
@@ -1,8 +1,34 @@
 /* eslint-disable no-console */
 import { DataSource } from 'apollo-datasource';
 import {Client} from 'memjs';
+import genericPool from 'generic-pool'
 
-let CLIENT;
+
+const maxConnections = 
+  process.env.MAX_MEMCACHE_CONNECTIONS ?
+    parseInt(process.env.MAX_MEMCACHE_CONNECTIONS) :
+    20
+
+const pool = genericPool.createPool({
+  create() {
+    return Promise.resolve(
+      Client.create(process.env.MEMCACHEDCLOUD_SERVERS, {
+        username: process.env.MEMCACHEDCLOUD_USERNAME,
+        password: process.env.MEMCACHEDCLOUD_PASSWORD,
+        retries: 0,
+        keepAlive: true,
+      })
+    )
+  },
+  async destroy(client) {
+    await client.quit()
+  },
+},
+{
+  min: 1,
+  max: maxConnections,
+  autostart: true,
+})
 
 const parseKey = (key) => {
   if (Array.isArray(key)) {
@@ -12,47 +38,34 @@ const parseKey = (key) => {
 };
 
 export default class Cache extends DataSource {
-  constructor(...args) {
-    super(...args);
-    // Memoize the REDIS instance so the connection can be resued.
-    if (!CLIENT) {
-      CLIENT = Client.create(process.env.MEMCACHEDCLOUD_SERVERS, {
-        username: process.env.MEMCACHEDCLOUD_USERNAME,
-        password: process.env.MEMCACHEDCLOUD_PASSWORD,
-        retries: 0,
-        keepAlive: true,
-      });
-    }
-    this.client = CLIENT;
-  }
 
   // 24 hours in seconds.
   DEFAULT_TIMEOUT = 86400;
 
   safely = async (func) => {
-    if (!this.client) return null;
-    try {
-      // Redundent assignment because it makes sure the error is captured.
-      const result = await func();
-      return result;
-    } catch (e) {
-      console.error(e);
-      return null;
-    }
+    return await pool.use(async (client) => {
+      try {
+        const result = await func(client);
+        return result;
+      } catch (e) {
+        console.error(e);
+        return null;
+      }
+    })
   };
 
   async set({ key, data, expires = this.DEFAULT_TIMEOUT }) {
-    return this.safely(() => {
+    return this.safely((client) => {
       if (data === undefined || data === null) {
-        return this.client.delete(parseKey(key))
+        return client.delete(parseKey(key))
       }
-      return this.client.set(parseKey(key), JSON.stringify(data), { expires })
+      return client.set(parseKey(key), JSON.stringify(data), { expires })
     });
   }
 
   async get({ key }) {
-    return this.safely(async () => {
-      const data = await this.client.get(parseKey(key));
+    return this.safely(async (client) => {
+      const data = await client.get(parseKey(key));
       if (!data.value) {
         return null
       }
