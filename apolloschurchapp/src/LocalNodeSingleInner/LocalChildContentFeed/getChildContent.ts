3a3ab596313e435db591857dc81e0198d4d603d5
Gordon <gburgett@watermark.org>
2022-03-16 10:49:22 -0500

    Add events to speaker detail

--- apolloschurchapp/src/LocalNodeSingleInner/LocalChildContentFeed/getChildContent.ts
+++ apolloschurchapp/src/LocalNodeSingleInner/LocalChildContentFeed/getChildContent.ts
@@ -1,40 +1,39 @@
 import { gql } from '@apollo/client';
 
 export default gql`
+  fragment localChildEventCollectionFragment on Local_EventCollection {
+    items {
+      sys {
+        id
+      }
+      title
+      description
+      eventType
+      startTime
+      endTime
+      art {
+        url
+      }
+    }
+  }
+
   query getChildContentLocal($itemId: ID!) {
     local @client {
       entry(id: $itemId) {
         __typename
         ... on Local_Breakouts {
           events: breakouts {
-            items {
-              sys {
-                id
-              }
-              title
-              description
-              eventType
-              art {
-                url
-              }
-            }
+            ...localChildEventCollectionFragment
           }
         }
         ... on Local_Track {
           events: scheduleItems {
-            items {
-              sys {
-                id
-              }
-              title
-              description
-              eventType
-              startTime
-              endTime
-              art {
-                url
-              }
-            }
+            ...localChildEventCollectionFragment
+          }
+        }
+        ... on Local_Speaker {
+          events: talks {
+            ...localChildEventCollectionFragment
           }
         }
       }
