fd653b768b3bafb64e94591cd89ed03abbe3e691
Gordon <gburgett@watermark.org>
2022-04-06 13:26:02 -0500

    Fix backup/restore of content on android

--- apolloschurchapp/shim.js
+++ apolloschurchapp/shim.js
@@ -1,3 +1,4 @@
+import "@azure/core-asynciterator-polyfill";
 
 // Android doesn't have BigInt type by default, and some deep node library requires it...
 if (typeof BigInt === 'undefined') global.BigInt = require('big-integer');
