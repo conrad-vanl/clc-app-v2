100984b3f3e07a443dba0f8fb240fd68b369ff1f
Gordon <gburgett@watermark.org>
2023-02-28 12:12:41 -0600

    remove remaining storybook stuff

--- apolloschurchapp/index.js
+++ apolloschurchapp/index.js
@@ -1,21 +1,10 @@
 import './shim';
 import './loadConfig';
 import { AppRegistry, YellowBox } from 'react-native';
-import ApollosConfig from '@apollosproject/config';
-
-// temp fix for the promise.finally
-// https://github.com/storybookjs/storybook/issues/8371
-const fn = Promise.prototype.finally;
-const Storybook = require('./storybook').default; // eslint-disable-line
-Promise.prototype.finally = fn; // eslint-disable-line
-const useStorybook = ApollosConfig.STORYBOOK === 'true';
 
 const MainApp = require('./src').default;
 
 let App = MainApp;
-if (useStorybook) {
-  App = Storybook;
-}
 
 YellowBox.ignoreWarnings([
   'Warning: isMounted(...) is deprecated',
